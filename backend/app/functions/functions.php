bba2217e7f84fb5db92286d4bbc97cc501260917
BrendoKildereSilva <kilderebrendo8080@gmail.com>
2023-07-20 16:51:06 -0300

    criando um controller de functions e adicionando uma função chamada urls para melhorar o resgate das urls

---
+++ backend/app/functions/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\functions;
+use urls;
+
+class functions{
+
+    static function urls(){
+        $todas_URLS = [
+            "banco-de-imagens" => [
+                "foto_perfil_users" => "http://localhost:8080/banco-de-imagens/foto_perfil_users/"
+            ], 
+        ];
+
+
+        return $todas_URLS;
+        
+        
+    }
+    
+    
+}
