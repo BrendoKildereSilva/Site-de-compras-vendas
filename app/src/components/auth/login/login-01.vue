8f921a6f18cc8c122acedb3274cbaa6b5b7497f5
BrendoKildereSilva <kilderebrendo8080@gmail.com>
2023-07-20 17:34:02 -0300

    Excluindo css dos inputs para padronizar no site inteiro, no main.css

--- app/src/components/auth/login/login-01.vue
+++ app/src/components/auth/login/login-01.vue
@@ -61,33 +61,6 @@ export default {
 <style lang="scss" scoped>
     
 
-    
-
-    input{
-        width: 99%;
-        padding: 0.1rem;
-        height: 3rem;
-        outline: none;
-        border: 0px;
-        border: 0.1rem solid $primary01;
-        border-radius: 0.6rem;
-        transition: 0.4s;
-        color: #707070;
-    }
-
-    input:focus{
-        border: 0.1rem solid $primary01;
-        transition: 0.4s;
-    }
-
-    input:focus + label{
-        color: $primary01;
-    }
-
-    label{
-        color: $primary01;
-    }
-
     .container-inputs{
         div{
             margin: 3rem 0rem;
